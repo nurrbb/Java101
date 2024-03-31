485fca7ef1d42b3d3228a4fee3b608e15cf558fc
nurrbb <enurb320@gmailc.om>
2024-03-31 21:17:59 +0300

    Warm-up rounds.

---
+++ CircleSliceCalculator/src/Main.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        int r,a;
+        double area, pi=3.14;
+
+        Scanner input = new Scanner(System.in);
+        System.out.print("Please enter radius: ");
+        r = input.nextInt();
+        System.out.print("Please enter central angle of the area to be calculated: ");
+        a=input.nextInt();
+        area = (pi*(r*r)*a)/360;
+        System.out.println("Area of "+a+" angle: "+area);
+
+        }
+    }
