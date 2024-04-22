484d3e7fd5f7015f9bf3e76192446f521fd4e808
nurrbb <enurb320@gmailc.om>
2024-04-22 14:19:15 +0300

    Warm-up

--- BoxingMatch/src/Main.java
+++ BoxingMatch/src/Main.java
@@ -2,7 +2,7 @@
 public class Main {
     public static void main(String[] args) {
 
-        Boxer mike = new Boxer("Mike Tyson",16,40,95,100);
+        Boxer mike = new Boxer("Mike Tyson",16,40,95,50);
         Boxer rocky = new Boxer("Rocky Marciano",10,100,95,20);
 
         Ring match = new Ring(mike,rocky,90,120);
--- BoxingMatch/src/Ring.java
+++ BoxingMatch/src/Ring.java
@@ -1,79 +1,61 @@
 public class Ring {
-    Boxer f1;
-    Boxer f2;
-    int minWeight;
-    int maxWeight;
+    private Boxer f1;
+    private Boxer f2;
+    private int minWeight;
+    private int maxWeight;
 
-    Ring(Boxer f1, Boxer f2, int minWeight, int maxWeight) {
+    public Ring(Boxer f1, Boxer f2, int minWeight, int maxWeight) {
         this.f1 = f1;
         this.f2 = f2;
-        this.maxWeight = maxWeight;
         this.minWeight = minWeight;
+        this.maxWeight = maxWeight;
     }
 
     public void start() {
-        if (checkWeight()) {
-            if (firstFighter() == f1) {
-                while (this.f1.health > 0 && this.f2.health > 0) {
-                    System.out.println("****NEW ROUND****");
-                    this.f2.health = f1.punch(this.f2);
-                    if (winner()) {
-                        break;
-                    }
-                    this.f1.health = f2.punch(this.f1);
-                    if (winner()) {
-                        break;
-                    }
-                    System.out.println(this.f1.name+"Health: "+this.f1.health);
-                    System.out.println(this.f2.name+"Health: "+this.f2.health);
-
-                }
-            } else {
-                while (this.f1.health > 0 && this.f2.health > 0) {
-                    System.out.println("****NEW ROUND****");
-                    this.f1.health = f2.punch(this.f1);
-                    if (winner()) {
-                        break;
-                    }
-                    this.f2.health = f1.punch(this.f2);
-                    if (winner()) {
-                        break;
-                    }
-                    System.out.println(this.f1.name+"Health: "+this.f1.health);
-                    System.out.println(this.f2.name+"Health: "+this.f2.health);
-                }
-            }
-
-        } else {
+        if (!checkWeight()) {
             System.out.println("The weights of the athletes do not match.");
+            return;
         }
-    }
 
-    public Boxer firstFighter() {
-        if (Math.random() < 0.5) {
-            return f1;
-        } else {
-            return f2;
+        Boxer firstFighter = chooseFirstFighter();
+
+        while (f1.health > 0 && f2.health > 0) {
+            System.out.println("****NEW ROUND****");
+            f2.health = firstFighter.punch(f2);
+            if (winner()) break;
+            printScore();
+
+            f1.health = firstFighter == f1 ? f2.punch(f1) : f1.punch(f2);
+            if (winner()) break;
+            printScore();
         }
+        System.out.println("****END OF MATCH****");
     }
 
-    boolean checkWeight() {
-        return (this.f1.weight >= minWeight && this.f1.weight <= maxWeight) && (this.f2.weight >= minWeight && this.f2.weight <= maxWeight);
+    private Boxer chooseFirstFighter() {
+        return Math.random() < 0.5 ? f1 : f2;
     }
 
-    public boolean winner() {
-        if (f1.health == 0) {
+    private boolean checkWeight() {
+        return (f1.weight >= minWeight && f1.weight <= maxWeight) &&
+                (f2.weight >= minWeight && f2.weight <= maxWeight);
+    }
+
+    private boolean winner() {
+        if (f1.health <= 0) {
             System.out.println("Winner is: " + f2.name);
             return true;
-        } else if (f2.health == 0) {
-            System.out.println("Winner is " + f1.name);
+        } else if (f2.health <= 0) {
+            System.out.println("Winner is: " + f1.name);
+            return true;
         }
         return false;
     }
 
-    public void printScore() {
-        System.out.println("***");
-        System.out.println(f1.name + "Remain health: " + f1.health);
-        System.out.println(f2.name + "Remain health: " + f2.health);
+    private void printScore() {
+        System.out.println();
+        System.out.println(f1.name + " Remain health: " + f1.health);
+        System.out.println(f2.name + " Remain health: " + f2.health);
+        System.out.println();
     }
 }
