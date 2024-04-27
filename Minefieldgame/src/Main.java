81c3a484e3018f751c2e4ff45a53e48dea26994d
nurrbb <enurb320@gmailc.om>
2024-04-27 21:07:43 +0300

    Warm-up

--- Minefieldgame/src/Main.java
+++ Minefieldgame/src/Main.java
@@ -1,5 +1,54 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
+        Scanner input = new Scanner(System.in);
+        System.out.print("Please enter width:");
+        int width = input.nextInt();
+        System.out.print("Please enter height:");
+        int height = input.nextInt();
+
+        while (height < 2 || width < 2) {
+            System.out.print("Height or width should be minimum 2! Please enter again:");
+            System.out.print("Height:");
+            height = input.nextInt();
+            System.out.print("Width:");
+            width = input.nextInt();
+        }
+        int totalElements = width * height;
+        int mineCount = totalElements / 4;
+
+        char[][] grid = MineSweeper.placeStars(width, height, mineCount);
+        char[][] revealedGrid = MineSweeper.createRevealedGrid(width, height);
+        int remainingCells = totalElements - mineCount;
+
+        while (true) {
+            MineSweeper.printGrid(revealedGrid);
+            System.out.print("Enter row:");
+            int row = input.nextInt()-1;
+            System.out.print("Enter column:");
+            int col = input.nextInt()-1;
+            if (MineSweeper.isValidCoordinate(row, col, width, height)) {
+                if (revealedGrid[row][col] != '-') {
+                    System.out.println("This coordinate has been already chosen. Please enter a new coordinate.");
+                    continue;
+                }
+                if (grid[row][col] == '*') {
+                    System.out.println("BOOM! Game Over.");
+                    break;
+                } else {
+                    int neighborMines = MineSweeper.countNeighborMines(grid, row, col);
+                    revealedGrid[row][col] = (char) (neighborMines + '0');
+                    remainingCells--;
+                    if (remainingCells == 0) {
+                        System.out.println("Congratulations! You have won the game!");
+                        break;
+                    }
+                }
+            } else {
+                System.out.println("Invalid coordinate. Please enter new coordinates.");
+            }
+        }
     }
 }
--- Minefieldgame/src/MineSweeper.java
+++ Minefieldgame/src/MineSweeper.java
@@ -1,2 +1,55 @@
-public class MineSweeper {
+class MineSweeper {
+    public static char[][] placeStars(int width, int height, int mineCount) {
+        char[][] grid = new char[height][width];
+
+        for (int i = 0; i < height; i++) {
+            for (int j = 0; j < width; j++) {
+                grid[i][j] = '-';
+            }
+        }
+        for (int k = 0; k < mineCount; k++) {
+            int randomRow = (int) (Math.random() * height);
+            int randomCol = (int) (Math.random() * width);
+            grid[randomRow][randomCol] = '*';
+        }
+
+        return grid;
+    }
+    public static char[][] createRevealedGrid(int width, int height) {
+        char[][] revealedGrid = new char[height][width];
+
+        for (int i = 0; i < height; i++) {
+            for (int j = 0; j < width; j++) {
+                revealedGrid[i][j] = '-';
+            }
+        }
+
+        return revealedGrid;
+    }
+    public static void printGrid(char[][] grid) {
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[i].length; j++) {
+                System.out.print(grid[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static boolean isValidCoordinate(int row, int col, int width, int height) {
+        return row >= 0 && row < height && col >= 0 && col < width;
+    }
+    public static int countNeighborMines(char[][] grid, int row, int col) {
+        int count = 0;
+        int[] dx = {0, 0, 1, -1, 1, -1, 1, -1};
+        int[] dy = {1, -1, 0, 0, 1, -1, -1, 1};
+        for (int i = 0; i < dx.length; i++) {
+            int newRow = row + dx[i];
+            int newCol = col + dy[i];
+            if (isValidCoordinate(newRow, newCol, grid[0].length, grid.length) && grid[newRow][newCol] == '*') {
+                count++;
+            }
+        }
+        return count;
+    }
+
 }
